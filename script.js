08920e0c613a37ae19645a509a51b383eefd81e3
DaxMac <daxmac.lab@gmail.com>
2026-07-24 10:56:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -101,3 +101,28 @@ document.querySelectorAll(".card").forEach(card => {
 
 
 console.log("DaxMac Website Loaded");
+
+       /* =======================================
+   Mobile Navigation
+======================================= */
+
+const menuToggle = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+menuToggle.addEventListener("click", () => {
+
+    navLinks.classList.toggle("active");
+
+});
+
+/* Close menu after clicking a link */
+
+document.querySelectorAll(".nav-links a").forEach(link => {
+
+    link.addEventListener("click", () => {
+
+        navLinks.classList.remove("active");
+
+    });
+
+});
